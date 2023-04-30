fb5fc06cfd4ec96f6fab2b4f70ae8d489e90b319
kitrath <crathmel@gmail.com>
2023-04-30 17:03:24 -0500

    add workbox service worker, add install.js (pwa events)

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,25 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Save prompt event and reveal install button
+    window.deferredPrompt = event;
+    butInstall.classList.toggle('hidden', false);
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    // if beforeinstallprompt hasn't fired for some reason, return
+    if (!promptEvent) {
+        return;
+    }
+    // Fire prompt
+    promptEvent.prompt();
+    window.deferredPrompt = null;
+    // Hide install button
+    butInstall.classList.toggle('hidden', true);
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
