d6d8b2ea5f89a5e869cbcc9941e2ec177b7949e5
kitrath <crathmel@gmail.com>
2023-05-01 14:46:00 -0500

    add app install button functionality

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,25 +1,35 @@
+const { WebpackOptionsDefaulter } = require("webpack");
+
 const butInstall = document.getElementById('buttonInstall');
 
+// Code source: https://pwa-workshop.js.org/5-pwa-install/#add-an-installation-button
+
+// Note: Added `hidden` attribute to a#butInstall in ../index.html
+
+let deferredPrompt;
 // Logic for installing the PWA
 window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
     // Save prompt event 
-    window.deferredPrompt = event;
+    deferredPrompt = event;
     // Ensure button is displayed in navbar
     butInstall.hidden = false;
+    butInstall.addEventListener("click", installJATE);
 });
 
-butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
-    // if beforeinstallprompt hasn't fired for some reason, return
-    if (!promptEvent) {
-        return;
+async function installJATE() {
+    // Show prompt
+    deferredPrompt.prompt();
+    butInstall.disabled = true;
+
+    const { outcome } = await deferredPrompt.userChoice;
+
+    if (outcome === "accepted") {
+        butInstall.hidden = true;
     }
-    // Fire prompt
-    promptEvent.prompt();
-    window.deferredPrompt = null;
-    // Hide install button
-    butInstall.hidden = true;
-});
+    butInstall.disabled = false;
+    deferredPrompt = null;
+}
 
 window.addEventListener('appinstalled', (event) => {
     window.deferredPrompt = null;
