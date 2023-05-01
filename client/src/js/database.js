0d6bd25fe9a7253e81cf60f0cef75309d1ebb39b
kitrath <crathmel@gmail.com>
2023-04-30 22:02:52 -0500

    add getDB and putDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,24 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB("jate", 1);
+  const tx = db.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  // We will always store all of the editor text, in a single "row"
+  const result = await store.put({ id: 1, content });
+  // Log the result
+  console.log("🚀 - data saved to the database", result.content);
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB("jate", 1);
+  const tx = db.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  // We will always retrieve the db's single "row"
+  const result = await store.get(1);
+  console.log("result.content:", result.content);
+  return result;
+}
 
 initdb();
